555ccb086787d5f6e000780263c67c6f5ddc5fcc
sannomanimani <gg361533652@gmail.com>
2026-01-18 19:50:08 +0800

    style(padding): 增加内容区域最大宽度至6xl

--- client/src/components/padding.tsx
+++ client/src/components/padding.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Padding({ className = "mx-4", children }: { className?: string, children?: React.ReactNode }) {
     return (
         <div className={`${className} sm:mx-6 md:mx-8 lg:mx-12 xl:mx-16 2xl:mx-24 duration-300`} >
-            <div className="max-w-4xl mx-auto w-full">
+            <div className="max-w-6xl mx-auto w-full">
                 {children}
             </div>
         </div>
